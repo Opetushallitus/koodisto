594c289788e5cb622adf30e912c7a79037846121
kallem <kalle.manninen@gofore.com>
2014-01-17 16:31:51 +0200

    KH-162: Koodin lisätiedot näkyviin koodin katselunäkymään

--- koodisto-ui/src/main/webapp/js/viewCodeElement.js
+++ koodisto-ui/src/main/webapp/js/viewCodeElement.js
@@ -10,7 +10,7 @@ app.factory('ViewCodeElementModel', function($location, $modal, CodeElementByUri
         this.deleteState = "disabled";
         this.alerts = [];
 
-        this.init = function(codeElementUri, codeElementVersion) {
+        this.init = function(scope, codeElementUri, codeElementVersion) {
 
             this.withinCodeElements = [];
             this.includesCodeElements = [];
@@ -18,11 +18,35 @@ app.factory('ViewCodeElementModel', function($location, $modal, CodeElementByUri
             this.deleteState = "disabled";
             this.alerts = [];
 
-            model.getCodeElement(codeElementUri, codeElementVersion);
+            model.getCodeElement(scope, codeElementUri, codeElementVersion);
         };
-        this.getCodeElement = function(codeElementUri, codeElementVersion) {
+        this.getCodeElement = function(scope, codeElementUri, codeElementVersion) {
             CodeElementByUriAndVersion.get({codeElementUri: codeElementUri, codeElementVersion: codeElementVersion}, function (result) {
                 model.codeElement = result;
+
+                scope.instructionsfi = model.languageSpecificValue(result.metadata, 'kayttoohje', 'FI');
+                scope.instructionssv = model.languageSpecificValue(result.metadata, 'kayttoohje', 'SV');
+                scope.instructionsen = model.languageSpecificValue(result.metadata, 'kayttoohje', 'EN');
+
+                scope.conceptfi = model.languageSpecificValue(result.metadata, 'kasite', 'FI');
+                scope.conceptsv = model.languageSpecificValue(result.metadata, 'kasite', 'SV');
+                scope.concepten = model.languageSpecificValue(result.metadata, 'kasite', 'EN');
+
+                scope.totakenoticeoffi = model.languageSpecificValue(result.metadata, 'huomioitavaKoodi', 'FI');
+                scope.totakenoticeofsv = model.languageSpecificValue(result.metadata, 'huomioitavaKoodi', 'SV');
+                scope.totakenoticeofen = model.languageSpecificValue(result.metadata, 'huomioitavaKoodi', 'EN');
+
+                scope.containssignificancefi = model.languageSpecificValue(result.metadata, 'sisaltaaMerkityksen', 'FI');
+                scope.containssignificancesv = model.languageSpecificValue(result.metadata, 'sisaltaaMerkityksen', 'SV');
+                scope.containssignificanceen = model.languageSpecificValue(result.metadata, 'sisaltaaMerkityksen', 'EN');
+
+                scope.doesnotcontainsignificancefi = model.languageSpecificValue(result.metadata, 'eiSisallaMerkitysta', 'FI');
+                scope.doesnotcontainsignificancesv = model.languageSpecificValue(result.metadata, 'eiSisallaMerkitysta', 'SV');
+                scope.doesnotcontainsignificanceen = model.languageSpecificValue(result.metadata, 'eiSisallaMerkitysta', 'EN');
+
+                scope.containscodesfi = model.languageSpecificValue(result.metadata, 'sisaltaaKoodiston', 'FI');
+                scope.containscodessv = model.languageSpecificValue(result.metadata, 'sisaltaaKoodiston', 'SV');
+                scope.containscodesen = model.languageSpecificValue(result.metadata, 'sisaltaaKoodiston', 'EN');
 
                 if (model.codeElement.tila === "PASSIIVINEN")
                     model.deleteState = "";
@@ -104,7 +128,7 @@ function ViewCodeElementController($scope, $location, $routeParams, ViewCodeElem
     $scope.model = ViewCodeElementModel;
     $scope.codeElementUri = $routeParams.codeElementUri;
     $scope.codeElementVersion = $routeParams.codeElementVersion;
-    ViewCodeElementModel.init($scope.codeElementUri, $scope.codeElementVersion);
+    ViewCodeElementModel.init($scope, $scope.codeElementUri, $scope.codeElementVersion);
 
 
     $scope.closeAlert = function(index) {
