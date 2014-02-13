994ea1c4cde9aed981254d442191d3b3c0803371
Antto Sierla <antto.sierla@gofore.com>
2014-02-13 15:30:54 +0200

    Access-Control-Allow-Origin konfiguroitavaksi

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/filter/CorsFilter.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/filter/CorsFilter.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Value;
 public class CorsFilter implements ContainerResponseFilter {
     @Value("${auth.mode}")
     private String authMode;
+    
+    @Value("${cors.allow-origin}")
+    private String allowOrigin;
 
     @Override
     public ContainerResponse filter(ContainerRequest containerRequest, ContainerResponse containerResponse) {
@@ -25,7 +28,14 @@ public class CorsFilter implements ContainerResponseFilter {
             }
         }
         if ("dev".equals(authMode)) {
+            // When testing on localhost, allow script access from all domains
             containerResponse.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        } else {
+            // Otherwise, allow only configured domains. Don't add header if no allowed domains
+            // configured (allows only same domain).
+            if (allowOrigin != null && !allowOrigin.isEmpty()) {
+                containerResponse.getHttpHeaders().add("Access-Control-Allow-Origin", allowOrigin);
+            }
         }
         return containerResponse;
     }
