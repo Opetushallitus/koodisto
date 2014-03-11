2736e56ef8b5956f5c3f22d5e0ee343f649de2a4
kallem <kalle.manninen@gofore.com>
2014-03-11 17:09:31 +0200

    KH-170 Korjataan Koodiston vienti CSV/XML

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/business/impl/KoodiBusinessServiceImpl.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/business/impl/KoodiBusinessServiceImpl.java
@@ -260,6 +260,7 @@ public class KoodiBusinessServiceImpl implements KoodiBusinessService {
                 }
             }
         }
+        koodisto.setPaivitysPvm(new Date());
     }
 
     @Override
