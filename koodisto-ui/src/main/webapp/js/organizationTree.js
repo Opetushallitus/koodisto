9326e107089e2577aa5a10fae0f7943488b11856
Humakt83 <risto.salama@cybercom.com>
2014-03-20 14:29:31 +0200

    Search organizations by skipping parents for OPH

--- koodisto-ui/src/main/webapp/js/organizationTree.js
+++ koodisto-ui/src/main/webapp/js/organizationTree.js
@@ -131,7 +131,7 @@ app.factory('OrganisaatioOPHTreeModel', function(Organizations, OrganizationByOi
         }
 
         instance.search = function(searchStr) {            
-            Organizations.get({"searchStr": searchStr}, function(result) {
+            Organizations.get({"searchStr": searchStr, "skipparents": true}, function(result) {
         	instance.model = result;
         	if(instance.model.organisaatiot.length < 4) {
         	    instance.model.organisaatiot.forEach(function(data) {
