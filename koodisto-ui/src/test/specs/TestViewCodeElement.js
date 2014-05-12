2c2106caf0e9852f00fbded40b6e830d03281854
Humakt83 <risto.salama@cybercom.com>
2014-05-12 13:44:52 +0300

    KH-194: Show version of referenced code relation
    In both view and edit code element views. Shows currently only the
    latest version, which is sufficient for edit view, but need to be
    modified for view view.

--- koodisto-ui/src/test/specs/TestViewCodeElement.js
+++ koodisto-ui/src/test/specs/TestViewCodeElement.js
@@ -1,6 +1,36 @@
 describe("Code Element View test", function() {
     
     var model, scope, mockBackend;
+    
+    var codesResponse = {
+		"koodistoUri" : "versiointitesti",
+		"resourceUri" : "http://koodistopalvelu.opintopolku.fi/versiointitesti",
+		"omistaja" : null,
+		"organisaatioOid" : "1.2.246.562.10.00000000001",
+		"lukittu" : null,
+		"latestKoodistoVersio" : {
+		    "versio" : 5,
+		    "paivitysPvm" : 1398346711637,
+		    "voimassaAlkuPvm" : "2014-04-24",
+		    "voimassaLoppuPvm" : null,
+		    "tila" : "HYVAKSYTTY",
+		    "version" : 2,
+		    "metadata" : [ {
+			"kieli" : "FI",
+			"nimi" : "Versiointitesti",
+			"kuvaus" : "Testataan versiointia",
+			"kayttoohje" : null,
+			"kasite" : null,
+			"kohdealue" : null,
+			"sitovuustaso" : null,
+			"kohdealueenOsaAlue" : null,
+			"toimintaymparisto" : null,
+			"tarkentaaKoodistoa" : null,
+			"huomioitavaKoodisto" : null,
+			"koodistonLahde" : null
+		    } ]
+		}
+	};
 
     beforeEach(module("koodisto", function ($provide) {
 	$provide.value('NoCacheInterceptor', {});
@@ -59,36 +89,6 @@ describe("Code Element View test", function() {
 	    }
 	};
 	
-	var codesResponse = {
-		"koodistoUri" : "versiointitesti",
-		"resourceUri" : "http://koodistopalvelu.opintopolku.fi/versiointitesti",
-		"omistaja" : null,
-		"organisaatioOid" : "1.2.246.562.10.00000000001",
-		"lukittu" : null,
-		"latestKoodistoVersio" : {
-		    "versio" : 5,
-		    "paivitysPvm" : 1398346711637,
-		    "voimassaAlkuPvm" : "2014-04-24",
-		    "voimassaLoppuPvm" : null,
-		    "tila" : "HYVAKSYTTY",
-		    "version" : 2,
-		    "metadata" : [ {
-			"kieli" : "FI",
-			"nimi" : "Versiointitesti",
-			"kuvaus" : "Testataan versiointia",
-			"kayttoohje" : null,
-			"kasite" : null,
-			"kohdealue" : null,
-			"sitovuustaso" : null,
-			"kohdealueenOsaAlue" : null,
-			"toimintaymparisto" : null,
-			"tarkentaaKoodistoa" : null,
-			"huomioitavaKoodisto" : null,
-			"koodistonLahde" : null
-		    } ]
-		}
-	};
-	
 	beforeEach(function() {
 	    //in order to get rid of controller's initialization
 	    mockBackend.expectGET(SERVICE_URL_BASE + "codeelement/versiointitesti_uudi/3").respond(givenResponseCodeElement())
@@ -104,6 +104,68 @@ describe("Code Element View test", function() {
 	
 	it("Editing latest version of code element is permitted", function() {
 	    expect(scope.model.editState).toEqual("");
+	})
+    })
+    
+    describe("Relations", function() {
+	
+	givenResponseCodeElementWithRelation = function() {
+	    mockBackend.expectGET(SERVICE_URL_BASE + "codes/versiointitesti").respond(codesResponse);
+	    return {
+		"koodiUri" : "versiointitesti_uudi",
+		"resourceUri" : "http://koodistopalvelu.opintopolku.fi/versiointitesti/koodi/versiointitesti_uudi",
+		"version" : 1,
+		"versio" : 3,
+		"koodisto" : {
+		    "koodistoUri" : "versiointitesti",
+		    "organisaatioOid" : "1.2.246.562.10.00000000001",
+		    "koodistoVersios" : [ 3 ]
+		},
+		"koodiArvo" : "uudi",
+		"paivitysPvm" : 1398346711458,
+		"voimassaAlkuPvm" : "2014-04-14",
+		"voimassaLoppuPvm" : null,
+		"tila" : "HYVAKSYTTY",
+		"metadata" : [ {
+		    "nimi" : "Uusinkoodi",
+		    "kuvaus" : "fawegag",
+		    "lyhytNimi" : "fweafaw",
+		    "kayttoohje" : null,
+		    "kasite" : null,
+		    "sisaltaaMerkityksen" : null,
+		    "eiSisallaMerkitysta" : null,
+		    "huomioitavaKoodi" : null,
+		    "sisaltaaKoodiston" : null,
+		    "kieli" : "FI"
+		} ],
+		"withinCodeElements" : [ "relaatiotesti" ],
+		"includesCodeElements" : [ ],
+		"levelsWithCodeElements" : [ ]
+	    }
+	};
+	
+	var codeElementRelationReferences = {
+		"koodisto" : {
+		    "koodistoUri" : "relaatiotestikoodisto"
+		},
+		"metadata" : [ {
+		    "nimi" : "Referenssi",
+		    "kieli" : "FI"
+		}],
+		"versio" : 2
+	}
+	
+	beforeEach(function() {
+	    mockBackend.expectGET(SERVICE_URL_BASE + "codeelement/versiointitesti_uudi/3").respond(givenResponseCodeElementWithRelation())
+	    mockBackend.expectGET(SERVICE_URL_BASE + "codeelement/latest/relaatiotesti").respond(codeElementRelationReferences);
+	    mockBackend.expectGET(SERVICE_URL_BASE + "codes/relaatiotestikoodisto").respond(codesResponse);
+	    mockBackend.flush();
+	})
+	
+	it("Should contain version number of code element relation references", function() {
+	    expect(model.withinCodeElements.length).toEqual(1);
+	    expect(model.withinCodeElements[0].versio).toEqual(2);
+	    expect(model.withinCodeElements[0].name).toEqual("Referenssi");
 	})
     })
     
