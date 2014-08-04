32faf9b66cb6aed4f6d88a93c958cb19691ff190
Humakt83 <risto.salama@cybercom.com>
2014-08-04 09:42:49 +0300

    OVT-7653: First draft for validator interface
    Intention is to validate arguments passed to REST-methods

---
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/RestValidator.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.koodisto.service.koodisto.rest.validator;
+
+public interface RestValidator<T> {
+    
+    void validateCreateNew(T validatable) throws IllegalArgumentException;
+    
+    void validateUpdate(T validatable) throws IllegalArgumentException;
+    
+    void validateDelete(T validatable) throws IllegalArgumentException;
+    
+    void validateGet(T validatable0) throws IllegalArgumentException;
+
+}
