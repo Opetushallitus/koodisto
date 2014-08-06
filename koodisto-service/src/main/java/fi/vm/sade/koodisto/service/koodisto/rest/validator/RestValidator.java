f2557e2971284a5b020b6128962cd353c5be89e5
Humakt83 <risto.salama@cybercom.com>
2014-08-06 14:30:05 +0300

    OVT-7653: Remove unnecessary throw declaration

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/RestValidator.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/RestValidator.java
@@ -2,12 +2,12 @@ package fi.vm.sade.koodisto.service.koodisto.rest.validator;
 
 public interface RestValidator<T> {
     
-    void validateCreateNew(T validatable) throws IllegalArgumentException;
+    void validateCreateNew(T validatable);
     
-    void validateUpdate(T validatable) throws IllegalArgumentException;
+    void validateUpdate(T validatable);
     
-    void validateDelete(T validatable) throws IllegalArgumentException;
+    void validateDelete(T validatable);
     
-    void validateGet(T validatable) throws IllegalArgumentException;
+    void validateGet(T validatable);
 
 }
--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/ValidatorUtil.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/ValidatorUtil.java
@@ -4,13 +4,13 @@ import com.google.common.base.Strings;
 
 public class ValidatorUtil {
     
-    public static void checkForNullOrEmpty(String toCheck, String error) throws IllegalArgumentException {
+    public static void checkForNullOrEmpty(String toCheck, String error) {
         if (Strings.isNullOrEmpty(toCheck)) {
             throw new IllegalArgumentException(error);
         }
     }
     
-    public static void checkForNull(Object toCheck, String error) throws IllegalArgumentException {
+    public static void checkForNull(Object toCheck, String error) {
         if (toCheck == null) {
             throw new IllegalArgumentException(error);
         }
