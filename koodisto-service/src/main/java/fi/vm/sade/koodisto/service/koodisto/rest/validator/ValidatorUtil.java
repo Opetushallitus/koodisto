15b5cfbffba942584f4b3c6887241f5a9116ed96
Humakt83 <risto.salama@cybercom.com>
2014-08-07 08:23:31 +0300

    OVT-7653: Use StringUtils instead of Strings

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/ValidatorUtil.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/ValidatorUtil.java
@@ -1,11 +1,11 @@
 package fi.vm.sade.koodisto.service.koodisto.rest.validator;
 
-import com.google.common.base.Strings;
+import org.apache.commons.lang.StringUtils;
 
 public class ValidatorUtil {
     
     public static void checkForNullOrEmpty(String toCheck, String error) {
-        if (Strings.isNullOrEmpty(toCheck)) {
+        if (StringUtils.isBlank(toCheck)) {
             throw new IllegalArgumentException(error);
         }
     }
