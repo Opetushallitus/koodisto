2c134967069b3403803b9c9306dc4507cb2c05ab
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2014-09-01 15:42:46 +0300

    OVT-7653: Fixed tests for new Responses, added Validators for relation list and extended codeelement.

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/ExtendedCodeElementValidator.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/koodisto/rest/validator/ExtendedCodeElementValidator.java
@@ -5,20 +5,20 @@ import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fi.vm.sade.koodisto.dto.KoodiDto;
+import fi.vm.sade.koodisto.dto.ExtendedKoodiDto;
 import fi.vm.sade.koodisto.model.KoodiMetadata;
 import fi.vm.sade.koodisto.service.business.exception.KoodiNimiEmptyException;
-import fi.vm.sade.koodisto.service.business.exception.KoodiUriEmptyException;
 import fi.vm.sade.koodisto.service.business.exception.MetadataEmptyException;
 import fi.vm.sade.koodisto.service.koodisto.rest.validator.Validatable.ValidationType;
 
-public class CodeElementValidator implements RestValidator<KoodiDto> {
+public class ExtendedCodeElementValidator implements RestValidator<ExtendedKoodiDto> {
 
     private static final KoodistoValidationException TO_THROW = new KoodistoValidationException("error.validation.codeelement");
+
     private Logger logger = LoggerFactory.getLogger(getClass());
-    
+
     @Override
-    public void validate(KoodiDto validatable, ValidationType type) {
+    public void validate(ExtendedKoodiDto validatable, ValidationType type) {
         if (type == ValidationType.INSERT) {
             validateInsert(validatable);
         } else {
@@ -27,9 +27,23 @@ public class CodeElementValidator implements RestValidator<KoodiDto> {
     }
 
     @Override
-    public void validateInsert(KoodiDto validatable) {
+    public void validateInsert(ExtendedKoodiDto validatable) {
         try {
             ValidatorUtil.checkForNull(validatable, TO_THROW);
+
+            ValidatorUtil.checkForNull(validatable.getIncludesCodeElements(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getLevelsWithCodeElements(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getWithinCodeElements(), TO_THROW);
+
+            ValidatorUtil.checkForBlank(validatable.getKoodiUri(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getVoimassaAlkuPvm(), TO_THROW);
+            ValidatorUtil.checkForBlank(validatable.getKoodiArvo(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getVersio(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getVersion(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getTila(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getMetadata(), TO_THROW);
+            ValidatorUtil.checkForNull(validatable.getKoodisto(), TO_THROW);
+
             checkMetadatas(validatable.getMetadata());
         } catch (Exception e) {
             throw new KoodistoValidationException(e.getMessage(), e);
@@ -37,14 +51,8 @@ public class CodeElementValidator implements RestValidator<KoodiDto> {
     }
 
     @Override
-    public void validateUpdate(KoodiDto validatable) {
-        try {
-            ValidatorUtil.checkForNull(validatable, TO_THROW);
-            ValidatorUtil.checkForBlank(validatable.getKoodiUri(), new KoodiUriEmptyException());
-            checkMetadatas(validatable.getMetadata());
-        } catch (Exception e) {
-            throw new KoodistoValidationException(e.getMessage(), e);
-        }
+    public void validateUpdate(ExtendedKoodiDto validatable) {
+        validateInsert(validatable);
     }
 
     private void checkRequiredMetadataFields(Collection<KoodiMetadata> metadatas) {
@@ -59,7 +67,5 @@ public class CodeElementValidator implements RestValidator<KoodiDto> {
         ValidatorUtil.checkCollectionIsNotNullOrEmpty(metadatas, new MetadataEmptyException());
         checkRequiredMetadataFields(metadatas);
     }
-
-
 
 }
