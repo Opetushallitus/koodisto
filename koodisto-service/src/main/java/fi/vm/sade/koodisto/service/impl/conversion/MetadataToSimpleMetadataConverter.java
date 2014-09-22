c20617b0704c699071c02929968f2407fd9847f8
Humakt83 <risto.salama@cybercom.com>
2014-09-22 15:15:47 +0300

    KH_203: Conversion from KoodiMetadata to SImpleKoodiMetadata

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/impl/conversion/MetadataToSimpleMetadataConverter.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/impl/conversion/MetadataToSimpleMetadataConverter.java
@@ -1,5 +1,9 @@
 package fi.vm.sade.koodisto.service.impl.conversion;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fi.vm.sade.koodisto.dto.SimpleKoodiMetadataDto;
 import fi.vm.sade.koodisto.dto.SimpleMetadataDto;
 import fi.vm.sade.koodisto.model.KoodiMetadata;
 import fi.vm.sade.koodisto.model.KoodistoMetadata;
@@ -13,5 +17,17 @@ public class MetadataToSimpleMetadataConverter {
     
     public static SimpleMetadataDto convert(KoodiMetadata metadata) {
         return new SimpleMetadataDto(metadata.getNimi(), metadata.getKieli(), metadata.getKuvaus());
+    }
+    
+    public static SimpleKoodiMetadataDto convertToSimpleKoodiMetadata(KoodiMetadata metadata) {
+        return new SimpleKoodiMetadataDto(metadata.getNimi(), metadata.getKieli(), metadata.getKuvaus(), metadata.getLyhytNimi());
+    }
+    
+    public static List<SimpleKoodiMetadataDto> convertToSimpleKoodiMetadata(KoodiMetadata ... koodiMetadatas) {
+        List<SimpleKoodiMetadataDto> metadatas = new ArrayList<>();
+        for (KoodiMetadata md : koodiMetadatas) {
+            metadatas.add(new SimpleKoodiMetadataDto(md.getNimi(), md.getKieli(), md.getKuvaus(), md.getLyhytNimi()));
+        }
+        return metadatas;
     }
 }
