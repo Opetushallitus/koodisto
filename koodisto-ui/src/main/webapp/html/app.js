9ce3be5c0f39b53a5b4721941660203a98ef114c
Mikko Apo <mikko.apo@reaktor.fi>
2016-01-27 18:30:53 +0200

    koodisto: Removed tarjonta-specific code from CSRF and Caller-ID header calls

--- koodisto-ui/src/main/webapp/html/app.js
+++ koodisto-ui/src/main/webapp/html/app.js
@@ -51,29 +51,16 @@ app.factory('NoCacheInterceptor', function() {
     };
 });
 
-app.factory('ajaxInterceptor', function(Config, $cookies) {
-    'use strict';
-    return {
-        request: function(config) {
-            if (callerid) {
-                config.headers['Caller-Id'] = "koodisto.koodisto-ui.frontend";
-            }
-            if($cookies['CSRF']) {
-                config.headers['X-CSRF'] = $cookies['CSRF'];
-            }
-            // Fix IE caching AJAX-requests
-            if (config.method === 'GET' && config.url.indexOf('/tarjonta-service/') !== -1) {
-                config.headers['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-            }
-            return config;
-        }
-    };
-})
+app.run(function($http, $cookies) {
+    $http.defaults.headers.common['Caller-Id'] = "koodisto.koodisto-ui.frontend";
+    if($cookies['CSRF']) {
+        $http.defaults.headers.common['X-CSRF'] = $cookies['CSRF'];
+    }
+});
 
 // Route configuration
 app.config([ '$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
     $httpProvider.interceptors.push('NoCacheInterceptor');
-    $httpProvider.interceptors.push('ajaxInterceptor');
     $routeProvider.
     // front page
     when('/etusivu', {
