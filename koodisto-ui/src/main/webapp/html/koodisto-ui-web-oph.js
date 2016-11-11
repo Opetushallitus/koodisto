f9a91f2b661132451989a31fbdc6a20d3f7fd993
Mikko Apo <mikko.apo@reaktor.fi>
2016-11-11 16:32:55 +0200

    koodisto-service.base includes /rest/

--- koodisto-ui/src/main/webapp/html/koodisto-ui-web-oph.js
+++ koodisto-ui/src/main/webapp/html/koodisto-ui-web-oph.js
@@ -2,7 +2,7 @@ window.urls.addProperties({
   "organisaatio-service.byOid": "/organisaatio-service/rest/organisaatio/:oid",
   "organisaatio-service.hae": "/organisaatio-service/rest/organisaatio/hae",
   "organisaatio-service.parentoids": "/organisaatio-service/rest/organisaatio/$1/parentoids",
-  "koodisto-service.base": "/koodisto-service/",
-  "koodisto-service.codesgroup": "/koodisto-service/codesgroup",
+  "koodisto-service.base": "/koodisto-service/rest/",
+  "koodisto-service.codesgroup": "/koodisto-service/rest/codesgroup",
   "cas.myroles": "/cas/myroles"
 });
