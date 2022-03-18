161f304e3d5809a64462f11dbe0b9063b5b12d75
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-03-18 10:18:32 +0200

    move webpack files

---
+++ koodisto-ui/webpack.make.js
@@ -0,0 +1,226 @@
+'use strict';
+
+// Modules
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractingPlugin = require('mini-css-extract-plugin');
+
+const convert = require('koa-connect');
+const history = require('connect-history-api-fallback');
+const proxy = require('http-proxy-middleware');
+
+module.exports = function makeWebpackConfig(options) {
+    /**
+     * Environment type
+     * BUILD is for generating minified builds
+     */
+    var BUILD = !!options.BUILD;
+
+    /**
+     * Config
+     * Reference: http://webpack.github.io/docs/configuration.html
+     * This is the object where all configuration gets set
+     */
+    var config = {};
+
+    config.mode = process.env.WEBPACK_SERVE ? 'development' : 'production';
+
+    /**
+     * Entry
+     * Reference: http://webpack.github.io/docs/configuration.html#entry
+     * Should be an empty object if it's generating a test build
+     * Karma will set this when it's a test build
+     */
+    config.entry = {
+        app: './src/main/webapp/html/app.js'
+    }
+
+    /**
+     * Output
+     * Reference: http://webpack.github.io/docs/configuration.html#output
+     * Should be an empty object if it's generating a test build
+     * Karma will handle setting it up for you when it's a test build
+     */
+        config.output = {
+            // Absolute output directory
+            path: BUILD ? __dirname + '/build' : __dirname + '/html',
+
+            // Output path from the view of the page
+            // Uses webpack-dev-server in development
+            publicPath: BUILD ? '/html/' : 'http://localhost:3000/',
+
+            // Filename for entry points
+            // Only adds hash in build mode
+            filename: BUILD ? '[name].[hash].js' : '[name].bundle.js',
+
+            // Filename for non-entry points
+            // Only adds hash in build mode
+            chunkFilename: BUILD ? '[name].[hash].js' : '[name].bundle.js'
+        }
+
+    if (!BUILD) {
+        config.serve = {
+            content: [__dirname],
+            add: (app, middleware, options) => {
+                app.use(convert(proxy('/configuration', { target: 'http://localhost:8081' })));
+                app.use(convert(history()));
+            },
+        }
+    }
+
+    /**
+     * Devtool
+     * Reference: http://webpack.github.io/docs/configuration.html#devtool
+     * Type of sourcemap to use per build type
+     */
+    if (BUILD) {
+        config.devtool = 'source-map';
+    } else {
+        config.devtool = 'eval';
+    }
+
+    /**
+     * Loaders
+     * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+     * List: http://webpack.github.io/docs/list-of-loaders.html
+     * This handles most of the magic responsible for converting modules
+     */
+
+    // Initialize module
+    config.module = {
+        rules: [
+            {
+                // JS LOADER
+                // Reference: https://github.com/babel/babel-loader
+                // Transpile .js files using babel-loader
+                // Compiles ES6 and ES7 into ES5 code
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                // ASSET LOADER
+                // Reference: https://github.com/webpack/file-loader
+                // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+                // Rename the file using the asset hash
+                // Pass along the updated reference to your code
+                // You can add here any file extension you want to get copied to your output
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file-loader'
+            },
+            {
+                // HTML LOADER
+                // Reference: https://github.com/webpack/raw-loader
+                // Allow loading html through js
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                // expose to window
+                test: require.resolve('jquery'),
+                use: [
+                    {loader: 'expose-loader', options: 'jQuery'},
+                    {loader: 'expose-loader', options: '$'},
+                ]
+            }
+        ],
+    };
+    config.resolve = {
+        alias: {
+            'jquery': require.resolve('jquery'),
+        }
+    };
+
+
+    // CSS LOADER
+    // Reference: https://github.com/webpack/css-loader
+    // Allow loading css through js
+    //
+    // Reference: https://github.com/postcss/postcss-loader
+    // Postprocess your css with PostCSS plugins
+    var cssLoader = {
+        test: /\.css$/,
+        // Reference: https://github.com/webpack/extract-text-webpack-plugin
+        // Extract css files in production builds
+        //
+        // Reference: https://github.com/webpack/style-loader
+        // Use style-loader in development for hot-loading
+        use: [
+            MiniCssExtractingPlugin.loader,
+            "css-loader",
+        ]
+    };
+
+    // Add cssLoader to the loader list
+    config.module.rules.push(cssLoader);
+
+    /**
+     * PostCSS
+     * Reference: https://github.com/postcss/autoprefixer-core
+     * Add vendor prefixes to your css
+     */
+    // config.postcss = [
+    //     autoprefixer({
+    //         browsers: ['last 2 version']
+    //     })
+    // ];
+
+    /**
+     * Plugins
+     * Reference: http://webpack.github.io/docs/configuration.html#plugins
+     * List: http://webpack.github.io/docs/list-of-plugins.html
+     */
+    config.plugins = [
+        // Reference: https://github.com/webpack/extract-text-webpack-plugin
+        // Extract css files
+        // Disabled when in test mode or not in build mode
+        new MiniCssExtractingPlugin({
+            fileName: '[name].[hash].css',
+            disable: !BUILD
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ];
+
+    // Skip rendering index.html in test mode
+    // Reference: https://github.com/ampedandwired/html-webpack-plugin
+    // Render index.html
+    config.plugins.push(
+        new HtmlWebpackPlugin({
+            template: './src/main/webapp/html/index.html',
+            inject: 'head',
+            minify: BUILD
+        })
+    );
+
+    // Add build specific plugins
+    if (BUILD) {
+        config.plugins.push(
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+            // Only emit files when there are no errors
+            new webpack.NoEmitOnErrorsPlugin(),
+        );
+        config.optimization = {
+            minimize: true,
+        }
+    }
+
+    /**
+     * Dev server configuration
+     * Reference: http://webpack.github.io/docs/configuration.html#devserver
+     * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+     */
+    config.devServer = {
+        contentBase: './public',
+        stats: {
+            modules: false,
+            cached: false,
+            colors: true,
+            chunk: false
+        }
+    };
+
+    return config;
+};
