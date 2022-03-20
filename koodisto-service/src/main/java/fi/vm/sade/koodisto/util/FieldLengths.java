215e7ff14ce97faaa5a88938c9b0ce66776031e7
mkamka <miikka.kajanne@wunderdog.fi>
2022-03-20 21:08:00 +0200

    vielä 1 smelli korjattu

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/util/FieldLengths.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/util/FieldLengths.java
@@ -1,6 +1,11 @@
 package fi.vm.sade.koodisto.util;
 
 public abstract class FieldLengths {
+
+    private FieldLengths() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final int DEFAULT_FIELD_LENGTH = 256;
     public static final int LONG_FIELD_LENGTH = 2048;
 }
