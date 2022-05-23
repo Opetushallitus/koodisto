ec47f0bd0b397a7e2c32c2bdb9e4af04f2c0c340
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-05-23 14:00:24 +0300

    conf and props

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/configuration/FlywayMigrationConfiguration.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/configuration/FlywayMigrationConfiguration.java
@@ -1,20 +1,16 @@
 package fi.vm.sade.koodisto.configuration;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class FlywayMigrationConfiguration {
     @Bean
-    public FlywayMigrationStrategy cleanMigrateStrategy() {
-        FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
-            @Override
-            public void migrate(Flyway flyway) {
-                flyway.repair();
-                flyway.migrate();
-            }
+    public FlywayMigrationStrategy repairStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
         };
-
-        return strategy;
     }
 }
