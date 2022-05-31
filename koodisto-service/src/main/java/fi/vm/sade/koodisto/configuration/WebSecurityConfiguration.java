9b1e7a21747123d8b4bc7a87410c3a54b8fbfc21
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-05-31 22:00:39 +0300

    remove unused imports

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/configuration/WebSecurityConfiguration.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/configuration/WebSecurityConfiguration.java
@@ -21,8 +21,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @Profile("!dev")
 @Configuration
