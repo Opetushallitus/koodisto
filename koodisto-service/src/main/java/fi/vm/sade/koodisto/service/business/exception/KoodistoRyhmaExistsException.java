d6d6626406e9c0d8cfc9e014f8ca44f01d650f4c
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-06-09 15:47:24 +0300

    remove copypasta

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/service/business/exception/KoodistoRyhmaExistsException.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/service/business/exception/KoodistoRyhmaExistsException.java
@@ -2,11 +2,6 @@ package fi.vm.sade.koodisto.service.business.exception;
 
 import fi.vm.sade.javautils.opintopolku_spring_security.SadeBusinessException;
 
-/**
- * Exception class for situations where koodisto nimi is empty or null
- *
- * @author wuoti
- */
 public class KoodistoRyhmaExistsException extends SadeBusinessException {
 
     private static final long serialVersionUID = 1L;
