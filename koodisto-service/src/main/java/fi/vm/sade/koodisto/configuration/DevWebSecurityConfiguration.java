a99e5f88890e85075f0288f8073a9948ee67066e
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-08-23 12:41:27 +0300

    security-configuration dev

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/configuration/DevWebSecurityConfiguration.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/configuration/DevWebSecurityConfiguration.java
@@ -7,20 +7,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Profile("dev")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = false, prePostEnabled = false, securedEnabled = true)
-public class DevWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class DevWebSecurityConfiguration {
 
     UserDetailsService userDetailsService;
     PasswordEncoder passwordEncoder;
@@ -39,16 +40,20 @@ public class DevWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return authProvider;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().authorizeRequests()
                 .antMatchers("/buildversion.txt").permitAll()
                 .antMatchers("/actuator/health").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/v3/api-docs/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/rest/**").permitAll()
-                .anyRequest().authenticated().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler())
-                .and().httpBasic();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .authenticationProvider(authProvider())
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+                .and().httpBasic().and().build();
     }
 
     @Bean
@@ -56,8 +61,4 @@ public class DevWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new CustomAccessDeniedHandler();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
-    }
 }
