e9b0f61f2cf24cbd25f674db571618b80f82b418
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2022-11-08 16:30:33 +0200

    global_exception_pass_exception let the Exception through, only logging the name of the exception

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/resource/advice/GlobalExceptionHandler.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/resource/advice/GlobalExceptionHandler.java
@@ -7,8 +7,7 @@ import fi.vm.sade.koodisto.service.business.exception.KoodiNotFoundException;
 import fi.vm.sade.koodisto.service.business.exception.KoodistoNotFoundException;
 import fi.vm.sade.koodisto.service.business.exception.KoodistoRyhmaNotEmptyException;
 import fi.vm.sade.koodisto.service.business.exception.KoodistoRyhmaNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
@@ -28,15 +27,15 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-    private static final String GENERIC_ERROR_CODE = "error.codes.generic";
     private static final String DEBUG_LOG_MESSAGE = "GlobalExceptionHandler handeled exception";
+    private static final String WARN_NOT_HANDELED_MESSAGE = "GlobalExceptionHandler passed this exception through {}";
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
         String errorMessage = e.getMessage();
         if (!violations.isEmpty()) {
@@ -49,88 +48,87 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<Object> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body("error.method.not.supported");
     }
 
     @ExceptionHandler(KoodistoNotFoundException.class)
     public ResponseEntity<Object> handleKoodistoNotFoundException(KoodistoNotFoundException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(KoodiNotFoundException.class)
     public ResponseEntity<Object> handleKoodiNotFoundException(KoodiNotFoundException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.badRequest().body("error.http.message.not.readable");
     }
 
     @ExceptionHandler(HttpMessageConversionException.class)
     public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageConversionException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.badRequest().body("error.http.message.not.convertable");
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<Object> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.badRequest().body("error.mediatype.not.supported");
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Object> handleIllegalArgumentException(MethodArgumentTypeMismatchException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.badRequest().body(String.format("error.validation.%s", e.getParameter().getParameterName()).toLowerCase());
     }
 
     @ExceptionHandler(SadeBusinessException.class)
     public ResponseEntity<Object> handleSadeBusinessException(SadeBusinessException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler(KoodistoRyhmaNotFoundException.class)
     public ResponseEntity<Object> handleSadeBusinessException(KoodistoRyhmaNotFoundException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         List<String> errors = e.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(NotAuthorizedException.class)
     public ResponseEntity<Object> handleNotAuthorizedException(NotAuthorizedException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("error.codes.insufficient.access.rights");
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public void handleAccessDeniedException(AccessDeniedException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         throw e;
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception e) {
-        logger.info(DEBUG_LOG_MESSAGE, e);
-        return ResponseEntity.internalServerError().body(GENERIC_ERROR_CODE);
-    }
-
-
     @ExceptionHandler(KoodistoRyhmaNotEmptyException.class)
     public ResponseEntity<Object> handleKoodistoRyhmaNotEmptyException(KoodistoRyhmaNotEmptyException e) {
-        logger.debug(DEBUG_LOG_MESSAGE, e);
+        log.debug(DEBUG_LOG_MESSAGE, e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("error.koodistoryhma.not.empty");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception e) throws Exception {
+        log.warn(WARN_NOT_HANDELED_MESSAGE, e.getClass().getName());
+        throw e;
     }
 
 }
