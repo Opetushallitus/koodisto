4df6e1f38bbd20c50324132f37f45f0526e17e89
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-04-04 12:22:04 +0300

    KH-414 Enable export task on any profile

--- koodisto-service/src/main/java/fi/vm/sade/koodisto/export/ExportService.java
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/export/ExportService.java
@@ -10,14 +10,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-@Profile("dev")
 public class ExportService {
     private final JdbcTemplate jdbcTemplate;
 
