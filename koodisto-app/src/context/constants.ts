da1de54b6aace3d0770627a5b5f93dc248248a0a
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-01-23 15:44:36 +0200

    OPHYK-220 run cypress tests in GA

--- koodisto-app/src/context/constants.ts
+++ koodisto-app/src/context/constants.ts
@@ -1,4 +1,5 @@
 export const ROOT_OID = '1.2.246.562.10.00000000001';
+export const BASE_PATH = '/koodisto-service/ui';
 export const API_LOKALISAATIO_PATH = '/lokalisointi/cxf/rest/v1/localisation';
 export const API_BASE_PATH = '/koodisto-service/rest';
 export const API_INTERNAL_PATH = '/koodisto-service/internal';
