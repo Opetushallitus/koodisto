9d3ec4176aef081f30c40be41636ec8902a710dd
Timo Suomela <timo.suomela@reaktor.com>
2025-02-15 11:44:46 +0200

    OPHYK-319 Add missing file

---
+++ koodisto-service/src/main/java/fi/vm/sade/koodisto/datantuonti/DatantuontiManifest.java
@@ -0,0 +1,15 @@
+package fi.vm.sade.koodisto.datantuonti;
+
+record DatantuontiManifest(String koodi,
+                           String koodimetadata,
+                           String koodinsuhde,
+                           String koodisto,
+                           String koodistometadata,
+                           String koodistonsuhde,
+                           String koodistoryhma,
+                           String koodistoryhma_koodisto,
+                           String koodistoryhmametadata,
+                           String koodistoversio,
+                           String koodistoversio_koodiversio,
+                           String koodiversio
+                           ) {}
