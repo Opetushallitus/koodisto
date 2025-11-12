f2f747918320d9059466bb6ae2cb9efec4f2eeb8
Timo Suomela <timo.suomela@reaktor.com>
2025-11-12 12:54:09 +0200

    OPHYK-886 Redirect /koodisto-service/ to /koodisto-service/ui, otherwise we get an empty page

---
+++ koodisto-service/src/test/java/fi/vm/sade/koodisto/UserInterfaceTest.java
@@ -0,0 +1,28 @@
+package fi.vm.sade.koodisto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserInterfaceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void servletContextRootshouldRedirectToUserInterface() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/ui"));
+    }
+}
